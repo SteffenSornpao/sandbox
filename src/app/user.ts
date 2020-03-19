ef6163e74652ba4f0a7b5c564d08c328653852b4
Steffen Sornpao <steffenssornpao@gmail.com>
2020-03-19 11:51:31 -0400

    create user list

--- src/app/user.ts
+++ src/app/user.ts
@@ -2,3 +2,19 @@ export interface User {
   id: number;
   name: string;
 }
+
+const generateNum = (max: number, min: number) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+export const USERS: User[] = [
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` },
+  { id: generateNum(99999, 10000), name: `User` }
+];
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,26 +1,39 @@
 import { Component, OnInit } from "@angular/core";
-import { User } from "../user";
+import { User, USERS } from "../user";
 
 @Component({
-  selector: "app-users",
-  templateUrl: "./users.component.html",
-  styles: [
-    `
-      p {
-        color: green;
-        span {
-          color: red;
-        }
-      }
-    `
-  ]
-  // styleUrls: ["./users.component.less"]
+  selector: "Users",
+  styleUrls: ["./users.component.less"],
+  // styles: [
+  //   `
+  //     p {
+  //       color: green;
+  //       span {
+  //         color: red;
+  //       }
+  //     }
+  //   `
+  // ],
+  templateUrl: "users.component.html"
+  // template: `
+  //   <ul>
+  //     <li *ngFor="let user of users" (click)="onSelect(user)">
+  //       <h2>{{ user.id }} - {{ user.name }}</h2>
+  //       <div>
+  //         <input [(ngModel)]="user.id" placeholder="id" />
+  //         <br />
+  //         <input [(ngModel)]="user.name" placeholder="name" />
+  //       </div>
+  //     </li>
+  //   </ul>
+  // `
 })
 export class UsersComponent implements OnInit {
-  user: User = {
-    id: 1,
-    name: "Me"
-  };
+  users = USERS;
+  selectedUser: User;
+  onSelect(user: User): void {
+    this.selectedUser = user;
+  }
   constructor() {}
 
   ngOnInit(): void {}
